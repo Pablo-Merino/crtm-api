a99daa0f5a943c92e5c6e32c027d512cad4dad62
Pablo Merino <pablo.perso1995@gmail.com>
2012-10-03 17:14:42 +0200

    First commit!

---
+++ application.rb
@@ -0,0 +1,35 @@
+# encoding: utf-8
+
+require 'sinatra'
+require 'haml'
+
+
+
+
+configure do
+	set :views, "#{File.dirname(__FILE__)}/views"
+end
+
+error do
+	@e = request.env['sinatra.error']
+	puts @e.backtrace.join("\n")
+	if ENV['RACK_ENV'] != "production"
+		haml :"errors/error", layout: :"errors/error_layout"
+	else
+		haml :"errors/error_dev", layout: false
+	end
+end
+
+helpers do
+
+	def partial(page, options={})
+		haml page.to_sym, options.merge!(:layout => false)
+	end
+	
+end
+
+get '/' do
+	
+	haml :index
+
+end
---
+++ config.ru
@@ -0,0 +1,12 @@
+# encoding: utf-8
+require File.join(File.dirname(__FILE__), 'application')
+
+set :run, false
+set :environment, :production
+
+FileUtils.mkdir_p 'log' unless File.exists?('log')
+log = File.new("log/sinatra.log", "a+")
+$stdout.reopen(log)
+$stderr.reopen(log)
+
+run Sinatra::Application
